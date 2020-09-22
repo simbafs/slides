8e887b51a9a552acb30a9b9e3d53185a7c96ca51
simba-fs <simba.fs@gmail.com>
2020-09-22 23:45:08 +0800

    basic functions finished

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const axios = require('axios').create({ timeout: 10000 });
 
 router.get('/', (req, res, next) => {
 	return res.render('index');
@@ -8,9 +9,17 @@ router.post('/s', (req, res, next) => {
 	let src = req.body.src;
 
 	// check src
-	if(!src || !src.match(/.*\/.*/)) return res.error('src format error');
+	if(!src || !src.match(/.*\/.*/)) return res.error('Src format error. It should match /.*\\/.*/');
+	res.redirect('/s/' + src);
+});
 
-
+router.get(/^\/s\/.*\/.*/, (req, res, next) => {
+	let src = req.path.split('/').slice(2);
+	if(src.length <= 2) src[2] = 'README.md';
+	let url = `https://raw.githubusercontent.com/${src[0]}/${src[1]}/master/${src.slice(2).join('/')}`
+	axios.head(url)
+		.then(e => 	res.render('slides', { url }))
+		.catch(e => res.error(`Can\'t find ${url}.`))
 });
 
 module.exports = router;
