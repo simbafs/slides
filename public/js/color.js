45cc948c95274a70a00c8bfc23c6770f38bb0c45
simba-fs <simba.fs@gmail.com>
2020-09-25 23:50:58 +0800

    change color

--- public/js/color.js
+++ public/js/color.js
@@ -3,6 +3,32 @@ var schemaClose = document.getElementById('schemaClose');
 var schemaPicker = document.getElementById('schemaPicker');
 var schemaSchema = document.getElementsByClassName('schemaSchema');
 var schemaStat = false;
+var dark = [
+	'black',
+	'blood',
+	'league',
+	'moon',
+	'night'
+];
+
+var form = document.getElementsByTagName('form')[0];
+var schema = 'white';
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	for(var i of e.target.elements){
+		if(i.checked) schema = i.value
+	}
+
+	// change button color
+	if(dark.includes(schema)){
+		schemaBtn.children[0].style.filter =  'invert(1)';
+	}else{
+		schemaBtn.children[0].style.filter =  'invert(0)';
+	}
+
+	// change css file
+	document.getElementById('themeCSS').href = `/reveal.js/dist/theme/${schema}.css`
+});
 
 schemaBtn.addEventListener('click', e => {
 	console.log(e);
