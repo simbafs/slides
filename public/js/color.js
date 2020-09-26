b5195633e4b8a5c1d26f0189eb7dd0553939a893
simba-fs <simba.fs@gmail.com>
2020-09-26 23:02:27 +0800

    auto load last theme

--- public/js/color.js
+++ public/js/color.js
@@ -1,42 +1,54 @@
-var schemaBtn = document.getElementById('schemaBtn');
-var schemaClose = document.getElementById('schemaClose');
-var schemaPicker = document.getElementById('schemaPicker');
-var schemaSchema = document.getElementsByClassName('schemaSchema');
-var schemaStat = false;
-var dark = [
-	'black',
-	'blood',
-	'league',
-	'moon',
-	'night'
-];
+var schema = (() => {
+	var schemaBtn = document.getElementById('schemaBtn');
+	var schemaClose = document.getElementById('schemaClose');
+	var schemaPicker = document.getElementById('schemaPicker');
+	var schemaSchema = document.getElementsByClassName('schemaSchema');
+	var schemaStat = false;
+	var dark = [
+		'black',
+		'blood',
+		'league',
+		'moon',
+		'night'
+	];
+	var form = document.getElementsByTagName('form')[0];
+	var schema = 'white';
 
-var form = document.getElementsByTagName('form')[0];
-var schema = 'white';
-form.addEventListener('submit', e => {
-	e.preventDefault();
-	for(var i of e.target.elements){
-		if(i.checked) schema = i.value
+	// set default theme, which gotten from localStrage
+	schemaToggle(localStorage.getItem('schema') || 'white');
+
+	function schemaToggle(schema){
+		// change button color
+		if(dark.includes(schema)){
+			schemaBtn.children[0].style.filter =  'invert(1)';
+		}else{
+			schemaBtn.children[0].style.filter =  'invert(0)';
+		}
+
+		// change css file
+		document.getElementById('themeCSS').href = `/reveal.js/dist/theme/${schema}.css`
+
+		localStorage.setItem('schema', schema);
 	}
 
-	// change button color
-	if(dark.includes(schema)){
-		schemaBtn.children[0].style.filter =  'invert(1)';
-	}else{
-		schemaBtn.children[0].style.filter =  'invert(0)';
-	}
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		for(var i of e.target.elements){
+			if(i.checked) schema = i.value
+		}
+		schemaToggle(schema);
+	});
 
-	// change css file
-	document.getElementById('themeCSS').href = `/reveal.js/dist/theme/${schema}.css`
-});
+	schemaBtn.addEventListener('click', e => {
+		console.log(e);
+		schemaStat = !schemaStat;
+		schemaPicker.classList.toggle('is:on', schemaStat);
+	});
 
-schemaBtn.addEventListener('click', e => {
-	console.log(e);
-	schemaStat = !schemaStat;
-	schemaPicker.classList.toggle('is:on', schemaStat);
-});
+	schemaClose.addEventListener('click', e => {
+		schemaStat = false;
+		schemaPicker.classList.toggle('is:on', false);
+	});
 
-schemaClose.addEventListener('click', e => {
-	schemaStat = false;
-	schemaPicker.classList.toggle('is:on', false);
-});
+	return schemaToggle;
+})();
