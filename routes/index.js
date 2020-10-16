dd79a9a0faaaa320cecdc1ceaf51a68ce0d8902b
simba-fs <simba.fs@gmail.com>
2020-10-16 23:49:02 +0800

    room

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/about', (req, res, next) => {
 
 router.use('/s', require('./slides'));
 router.use('/h', require('./help'));
-router.use('/room', require('./room'));
+router.use('/', require('./room'));
 
 module.exports = router;
 
--- routes/room.js
+++ routes/room.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const axios = require('axios').create({ timeout: 10000 });
 
 function random(len = 5){
 	let r = (lim = 10) => Math.floor(Math.random() * lim);
@@ -13,11 +14,11 @@ function random(len = 5){
 let room = [
 ];
 
-router.get('/', (req, res, next) => {
-	res.render('room');
+router.get('/room', (req, res, next) => {
+	res.render('newHost');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/room', (req, res, next) => {
 	let data = req.body;
 
 	do{
@@ -27,11 +28,25 @@ router.post('/', (req, res, next) => {
 
 	room.push(data);
 
-	res.redirect(`/room/${data.id}`);
+	res.redirect(`/host/${data.id}`);
 });
 
-router.get('/:id', (req, res, next) => {
-	res.json(req.params);
+router.get('/host/:id', (req, res, next) => {
+	let data = room.find(i => i.id === req.params.id);
+	let src = data.path.split('/');
+	console.log({src});
+	if(src.length <= 2) src[2] = 'README.md';
+	let originUrl = `https://github.com/${src[0]}/${src[1]}/blob/master/${src.slice(2).join('/')}`
+	let url = `https://raw.githubusercontent.com/${src[0]}/${src[1]}/master/${src.slice(2).join('/')}`
+	console.log({ url, originUrl });
+	axios.head(url)
+		.then(e => 	res.render('slides', { 
+			url, 
+			originUrl,
+			path: req.path.slice(1),
+			mode: 'host'
+		}))
+		.catch(e => res.error(`Can\'t find ${url}`))
 });
 
 function socket(io){
