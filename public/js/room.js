3d89bf50aa59e2b41629e4e3dad45d2a65a7f1cc
simba-fs <simba.fs@gmail.com>
2020-10-25 22:26:04 +0800

    working on /room and /host. It can currectly redirect

---
+++ public/js/room.js
@@ -0,0 +1,42 @@
+var step = (() => {
+	var toggle = [
+		document.querySelector('#createPath'),
+		document.querySelector('#createName'),
+		document.querySelector('#createKey'),
+		document.querySelector('#createSubmit')
+	];
+	var form = document.querySelector('#create form');
+	var steps = document.getElementById('createStep').children;
+	var nowStep = 0;
+
+	function step(n){
+		// check n
+		if(n > 3 || n < 0) return;
+
+		// check validation
+		if(toggle[nowStep].checkValidity()){
+			toggle[nowStep].classList.remove('is:danger');
+		}else{
+			toggle[nowStep].classList.add('is:danger');
+			return;
+		}
+
+		for(i in toggle){
+			if(i == n) steps[i].classList.add('is:on');
+			else steps[i].classList.remove('is:on');
+		}
+		toggle[n].focus ? toggle[n].focus() : null;
+		nowStep = n;
+	}
+
+	toggle.forEach((item, index) => item.addEventListener('focus', () => step(index)));
+
+	var query = location.search.slice(1).split('&').map(i => i.split('=')).reduce((a, b) => {a[b[0]] = b[1]; return a}, {});
+
+	if(query.path){
+		toggle[0].value = query.path
+		step(1);
+	}
+
+	return step;
+})();
--- routes/room.js
+++ routes/room.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
+// room
 router.get('/room', (req, res, next) => {
+	if(req.query.id) res.redirect(`/room/${req.query.id}`);
 	res.render('room');
+});
+
+router.get('/room/:id', (req, res, next) => {
+	res.render('room');
+});
+
+// host
+var hosts = [];
+
+
+router.post('/host', (req, res, next) => {
+	hosts.push(req.body);
+	res.json(hosts);
 });
 
 router.get('/host', (req, res, next) => {
