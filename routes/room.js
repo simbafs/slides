1684142629861fada59e9583064c1b422de28274
simba-fs <simba.fs@gmail.com>
2020-10-26 23:09:26 +0800

    finish room.ejs. Now working on /host

--- routes/room.js
+++ routes/room.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const random = require('../lib/random');
+const axios = require('axios').create({ timeout: 10000 });
 
 // room
 router.get('/room', (req, res, next) => {
@@ -13,14 +15,30 @@ router.get('/room/:id', (req, res, next) => {
 // host
 var hosts = [];
 
-
 router.post('/host', (req, res, next) => {
-	hosts.push(req.body);
-	res.json(hosts);
+	let data = {...req.body};
+
+	// parse data
+	data.id = random();
+	if(!data.key) data.key = random();
+	if(!data.name) data.name = data.path;
+
+	let src = data.path.split('/');
+	// https://github.com/simba-fs/slides-test/blob/master/folder1/folder2/slides.md
+	if(src.length <= 2) src[2] = 'README.md';
+	let originUrl = `https://github.com/${src[0]}/${src[1]}/blob/master/${src.slice(2).join('/')}`
+	let url = `https://raw.githubusercontent.com/${src[0]}/${src[1]}/master/${src.slice(2).join('/')}`
+	axios.head(url)
+		.then(() => res.redirect(`/host/${data.id}`))
+		.catch(e => res.error(`Can\'t find ${url}`))
+
+	hosts.push(data);
+	console.log(hosts);
 });
 
-router.get('/host', (req, res, next) => {
-
+router.get('/host/:id', (req, res, next) => {
+	let id = req.params.id;
+	res.json(id);
 });
 
 module.exports = router;
